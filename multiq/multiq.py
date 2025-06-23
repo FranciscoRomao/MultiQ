00cb8b8172c2e5f4692c31b6d9e4d29f2fca6dd8
Dan Vonk <dan@danvonk.com>
2025-06-23 15:49:15 +0200

    Code cleanup

--- multiq/multiq.py
+++ multiq/multiq.py
@@ -1,7 +1,5 @@
 import logging
-import json
 
-import zac.zac as zac
 from multiq.compiler.planner import Planner
 from multiq.compiler.orchestrator import Orchestrator
 from multiq.animator.animator import Animator
@@ -12,32 +10,13 @@ logger = logging.getLogger("multiq")
 
 class MultiQ:
     def __init__(self):
-        self.tiles = []
         self.config = MultiQConfig.from_config_file("config.yaml")
-
-    # TODO: remove, unused
-    def init_zac(self):
-        zac_settings = {
-            "routing_strategy": "maximalis",
-            "scheduling": "asap",
-            "trivial_placement": False,
-            "dynamic_placement": True,
-            "use_window": True,
-            "window_size": 1000,
-            "reuse": True
-        }
-        
-        zacc = zac.ZAC()
-        zacc.parse_setting(zac_settings)
-        zacc.set_architecture(self.arch)
-
-        return zacc
 
     def set_inputs(self, input_files: list[str]):
         
         self.planner = Planner(self.config)
         self.planner.set_input_circuits(input_files, optimization_level=3)
-        self.tiles = self.planner.set_best_architectures()
+        tiles = self.planner.set_best_architectures()
 
         self.selector = CircuitSASelector(self.config)
 
