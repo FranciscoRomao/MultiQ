466222a40ce1452fa419d14f7e1057033eca08f9
Dan Vonk <dan@danvonk.com>
2025-06-23 14:55:31 +0200

    Remove old code

--- multiq/multiq.py
+++ multiq/multiq.py
@@ -12,13 +12,8 @@ logger = logging.getLogger("multiq")
 
 class MultiQ:
     def __init__(self):
-        self.print_configuration()
         self.tiles = []
         self.config = MultiQConfig.from_config_file("config.yaml")
-
-    def print_configuration(self):
-        if self.config:
-            logger.info("Using parameters from config file.")
 
     # TODO: remove, unused
     def init_zac(self):
